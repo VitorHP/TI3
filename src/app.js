347208b0a4a59854e2ee63b1b006e0bf5854e94e
Vitor HP Bittencourt <vitorhp2@gmail.com>
2016-08-26 00:22:12 -0300

    Removed unused stuff

--- src/app.js
+++ src/app.js
@@ -1,39 +1,23 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import SC from 'node-soundcloud';
 import Loading from 'react-loading';
 
 var client_id = 'YOUR SOUNDCLOUD APP ID';
 
-SC.init({
-  id: client_id
-});
-
 class Main extends Component {
   constructor(props){
     super();
-
-    this.state = {
-      query: '',
-      hasResults: false,
-      searchResults: [],
-      isLoading: false
-    };
-  }
-
-  handleTextChange(event){
-    this.setState({
-      query: event.target.value
-    });
-    if(event.key === 'Enter'){
-      this.search.call(this);
-    }
   }
 
   render(){
     return (
-      <div>
-        <h1 className="app-title">AGAIN</h1>
+      <div className="row">
+        <div className="col-md-9 map">
+          <h1 className="app-title">Map</h1>
+        </div>
+        <div className="col-md-3 toolbox">
+          <h1 className="app-title">Toolbox</h1>
+        </div>
       </div>
     );
   }
